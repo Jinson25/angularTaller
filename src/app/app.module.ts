76699670935cd5bc7a2dfa9048764bde835c7d76
Jinson25 <am987406@gmail.com>
2023-05-17 19:35:54 -0500

    PRODUCTS, EDIT DELETE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
         BrowserModule,
         AppRoutingModule,
         PagesModule,
-        HttpClientModule
+        HttpClientModule,
+        FormsModule
 
     ]
 })
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -8,72 +8,64 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-   products:ProductModel[] = [];
+  products: ProductModel[] = [];
+  selectedProduct: UpdateProductDto = {};
 
-   selectedProduct: UpdateProductDto = {title:'', price:0, description:''};
+  constructor(private productService: ProductService) { }
+  showUpdateForm: boolean = false;
 
-  constructor(private productService:ProductService) {
-   this.editProduct();
-  }
-  
   ngOnInit(): void {
     this.getProducts();
-    //this.getProduct();
-    //this.createProduct();
-    //this.updateProduct();
-    //this.deleteProduct();
   }
 
-  getProducts(){
-    const url = "https://api.escuelajs.co/api/v1/products";
-    this.productService.getAll().subscribe(
-      response =>{
-        this.products = response;
-        console.log(response);
-      }
-    )
-  }
-  getProduct(id: ProductModel['id'] ){
-    const url = "https://api.escuelajs.co/api/v1/products/id";
-    return this.productService.getOne(id).subscribe(
-      response =>{
-        console.log(response);
-      }
-    )
+  getProducts(): void {
+    this.productService.getAll().subscribe(response => {
+      this.products = response;
+      console.log(response);
+    });
   }
 
-  createProduct(product: CreateProductDto){
-    const data = {
-      title: 'Cocinas',
-      price: 1100,
-      description: '6 quemadores / Jinson Medina',
-      images: [
-        'https://norte.dico.com.mx/media/catalog/product/cache/ddfdab190e55251eefb307178e423bf7/s/a/sala-modular-capital-calabaza-moderno-decorado-sal20754s1-2_1.jpg',
-      ],
+  createProduct() {
+    const data: CreateProductDto = {
+      title: "Aditamentos de cocina",
+      price: 25,
+      description: "Cosinero, arrozera",
+      images: ['http://api.lorem.space/image/furniture?w=640&h=480&r=8718'],
       categoryId: 1,
     };
-    this.productService.store(data).subscribe(
-      response=>{
-        console.log(response)})
-   }
-  editProduct(){
-    this.selectedProduct = {title:'', price:0, description:''};
+    
+    this.productService.store(data).subscribe(response => {
+      console.log(response);
+      // Llama a getProducts() nuevamente para obtener la lista actualizada después de crear el producto
+      this.getProducts();
+    });
   }
-  
-  updateProduct(){
-    const data = {
-      title: 'pc gaming',
-      price: 1800,
-      description: 'rgba rtx / Jinson Medina',
+
+  editProduct(product: ProductModel) {
+    this.selectedProduct = product;
+  }
+
+  updateProduct() {
+    const data: UpdateProductDto = {
+      title: "Pavilion",
+      price: 1500,
+      description: "Laptop Gaming / Jinson Medina",
+      images: ['http://api.lorem.space/image/furniture?w=640&h=480&r=8718'],
+      categoryId: 1
     };
-    this.productService.update(86,data).subscribe(
-      response => {
-        console.log(response)})
-   }
-   deleteProduct(id:ProductModel['id']){
-    this.productService.destroy(id).subscribe(
-      response => {
-        this.products= this.products.filter(product => product.id != id);
-        console.log(response)})
-   }
+    
+    const productId = this.selectedProduct.id; // Obtén el ID del producto seleccionado
+    this.productService.update(12, data).subscribe(response => {
+      console.log(response);
+      // Llama a getProducts() nuevamente para obtener la lista actualizada después de actualizar el producto
+      this.getProducts();
+    });
+  }
+
+  deleteProduct(id: ProductModel['id']) {
+    this.productService.destroy(id).subscribe(response => {
+      this.products = this.products.filter(product => product.id !== id);
+      console.log(response);
+    });
+  }
 }
